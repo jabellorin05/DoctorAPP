43bf44d95b0e51e35fc1343f5ce0f536994ae415
jabellorin05 <jabellorin@gmail.com>
2024-10-15 15:01:12 -0400

    button on the pdf

--- src/app/components/patient-search/patient-search.component.ts
+++ src/app/components/patient-search/patient-search.component.ts
@@ -17,7 +17,7 @@ export class PatientSearchComponent {
   searchCriteria: 'name' | 'id' = 'name'; // Por defecto buscar por nombre
   selectedPatient: Patient | null = null;  // Variable para almacenar el paciente seleccionado
   noPatientFound: boolean = false;  // Variable para manejar el mensaje de "No se encontró paciente"
-  
+  isLoading: Boolean = true;  // Variable para manejar el estado de carga
   
   @Output() patientSelected = new EventEmitter<number>();
 
@@ -26,6 +26,8 @@ export class PatientSearchComponent {
     private router: Router  // Inyectar Router
   ) {
     this.patients = this.patientService.getPatients();
+  
+   
   }
 
  // Método que se ejecuta al cambiar el criterio de búsqueda
@@ -62,8 +64,11 @@ onSelectPatient(patientId: number) {
   this.patientSelected.emit(patientId);
   // Buscar el paciente por ID y asignarlo a selectedPatient
   this.selectedPatient = this.patientService.findPatientById(patientId) || null;
-
   if (this.selectedPatient) {
+    this.getObservationNotes(this.selectedPatient.id);
+   
+  }
+  if (this.selectedPatient && this.isLoading) {
     this.router.navigate(['/observations', this.selectedPatient.id]);  // Navegar a observaciones con el ID del paciente
   }
 
@@ -77,16 +82,56 @@ generatePDF() {
   const data = document.getElementById('report-content');
   if (data) {
     html2canvas(data).then(canvas => {
-      const imgWidth = 208; // A4 size page width in mm
-      const pageHeight = 295; // A4 size page height in mm
-      const imgHeight = canvas.height * imgWidth / canvas.width;
-      const doc = new jsPDF('p', 'mm', 'a4');
-      const imgData = canvas.toDataURL('image/png');
-      doc.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
-      doc.save('patient-report.pdf');
+        const imgWidth = 208; // Ancho de la imagen en mm (A4 size)
+        const pageHeight = 295; // Altura de la página en mm (A4 size)
+        const imgHeight = canvas.height * imgWidth / canvas.width; // Calcula la altura en función del ancho
+        
+        const doc = new jsPDF('p', 'mm', 'a4');
+        const imgData = canvas.toDataURL('image/png');
+        
+        const margin = 10; // Margen deseado en mm
+        const xPos = (doc.internal.pageSize.getWidth() - imgWidth) / 2 + margin; // Añade margen a la posición X
+        const yPos = 0; // Mantén Y en 0 para que la imagen comience desde la parte superior
+
+        doc.addImage(imgData, 'PNG', xPos, yPos, imgWidth - 2 * margin, imgHeight); // Restamos el margen al ancho de la imagen
+        doc.save('patient-report.pdf');
     });
-  }
 }
 
+
+
+}
+  
+
+
+async getObservationNotes(id: number) { 
+
+  const url = `https://localhost:7288/api/Observation/PatientID/${id}`;
+
+   const response =  await fetch(url)
+    const data = await response.json()
+    if (this.selectedPatient) {
+      this.selectedPatient.observations = data;
+      this.getPrescriptionNotes(id);
+      this.isLoading = false;
+    }
+
+
+
+  }
+
+  async getPrescriptionNotes(id: number) { 
+
+    const url = `https://localhost:7288/api/Prescription/PatientID/${id}`;
+  
+     const response =  await fetch(url)
+      const data = await response.json()
+      if (this.selectedPatient) {
+        this.selectedPatient.prescriptions = data;
+      }
+  
+  
+  
+    }
 
 }
